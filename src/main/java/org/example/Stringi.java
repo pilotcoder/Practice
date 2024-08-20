03772cf9f77ae3394b7b2d218cefdf927620e7fa
Marcin <awings@o2.pl>
2024-08-20 02:28:20 +0200

    string

--- src/main/java/org/example/Stringi.java
+++ src/main/java/org/example/Stringi.java
@@ -18,7 +18,7 @@ public class Stringi {
         }
         return reversed;
     }
-//    sprawdzenie palindromu
+//    sprawdzenie palindromu....
     public static boolean isPalindrome(String string){
         boolean test = true;
         for (int i = 0; i < (string.length()-1)/2; i++) {
